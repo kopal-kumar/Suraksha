d9fbc0b8f915c552861a0b66144e2acbcaff39eb
kopal <kopalkumaar@gmail.com>
2020-07-16 10:23:11 -0500

    add 911 call

--- Suraksha/ViewController.swift
+++ Suraksha/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func call911(_ sender: Any) {
+        if let url = URL(string: "tel://6366279634"),
+        UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)}
+    
+    }
+    
+    
+    
+    
 }
 
